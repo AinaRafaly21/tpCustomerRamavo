06cfd714ce1a46c58e025054e40f421a736cb61e
AinaRafaly21 <Ramavo.nyaina@gmail.com>
2023-03-17 01:15:43 +0300

    Front

---
+++ src/main/java/mg/itu/tpcustomerramavo/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpcustomerramavo.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author Ny Aina Rafaly
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
