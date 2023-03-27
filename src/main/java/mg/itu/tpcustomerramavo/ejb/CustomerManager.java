5bc0c2f7220af2797b0e4cbf4f118f4853411b02
AinaRafaly21 <Ramavo.nyaina@gmail.com>
2023-03-27 23:18:38 +0300

    Finalisation

--- src/main/java/mg/itu/tpcustomerramavo/ejb/CustomerManager.java
+++ src/main/java/mg/itu/tpcustomerramavo/ejb/CustomerManager.java
@@ -24,6 +24,10 @@ public class CustomerManager {
        Query query = em.createNamedQuery("Customer.findAll");
        return query.getResultList();
     }
+    
+    public Customer findById(int idCustomer) {  
+        return em.find(Customer.class, idCustomer);  
+    }
 
     public Customer update(Customer customer) {
        return em.merge(customer);
